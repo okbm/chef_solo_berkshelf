c90f5bea0ee4d53593a28dcf77b1196b2ea33b99
Masayoshi Okabe <freezemoon.s@gmail.com>
2014-06-15 17:54:14 +0900

    javaのインストール追加

--- site-cookbooks/application/recipes/programing_lang.rb
+++ site-cookbooks/application/recipes/programing_lang.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
- %w{php5 php-pear php5-cli php5-common php5-dev php5-gd php5-mysql libapache2-mod-php5 php-apc}.each do |suffix|
+ %w{php5 php-pear php5-cli php5-common php5-dev php5-gd php5-mysql libapache2-mod-php5 php-apc openjdk-6-jre}.each do |suffix|
      package suffix do
          action :install
      end
